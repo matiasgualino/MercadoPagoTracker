cf7bd5acc19b7c70c69ba1ad09f613f55469087d
Matias Gualino <matias.gualino@gmail.com>
2016-05-13 10:31:14 -0300

    Change UA

--- MPTracker/GATracker.swift
+++ MPTracker/GATracker.swift
@@ -14,14 +14,8 @@ public class GATracker: NSObject {
     
     
     private override init(){
-        // Configure tracker from GoogleService-Info.plist.
-        /*var configureError:NSError?
-        GGLContext.sharedInstance().configureWithError(&configureError)
-        assert(configureError == nil, "Error configuring Google services: \(configureError)")*/
-        
-        // Optional: configure GAI options.
         let gai = GAI.sharedInstance()
-		gai.trackerWithTrackingId("UA-77498511-1")
+		gai.trackerWithTrackingId("UA-46087162-2")
         gai.trackUncaughtExceptions = true  // report uncaught exceptions
         gai.logger.logLevel = GAILogLevel.Verbose  // remove before app release
     }
--- MercadoPagoTracker.podspec
+++ MercadoPagoTracker.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "MercadoPagoTracker"
-  s.version          = "0.4.0"
+  s.version          = "0.5.0"
   s.summary          = "A MercadoPago Tracker SDK."
 
 # This description is used to generate tags and improve search results.
